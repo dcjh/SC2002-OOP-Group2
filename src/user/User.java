bf703c03ec34d6ba1cc9d2f6183b6fc2efca5850
jaycelimyi <143604419+jaycelimyi@users.noreply.github.com>
2024-10-16 15:09:47 +0800

    this is here for temp/ref usage, more time will be spend to make this better

--- src/user/User.java
+++ src/user/User.java
@@ -1,5 +1,29 @@
 package user;
+//this is here for temp/ref usage, more time will be spend to make this better
+public abstract class User {
+    protected String userID;
+    protected String password;
 
-public class User {
-    
+    // Constructor
+    public User(String userID, String password) {
+        this.userID = userID;
+        this.password = password;
+    }
+
+    // Common behavior: Login
+    public boolean login(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
+
+    // Abstract methods to be implemented by subclasses
+    public abstract void viewMenu(); // Each user will have a different menu
+
+    // Getters and Setters
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setPassword(String newPassword) {
+        this.password = newPassword;
+    }
 }
