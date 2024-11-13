157c1c317fdc6622928accc09bc7a7deb1a919f7
sherilyn7 <chuasherilyn247@gmail.com>
2024-11-13 23:37:34 +0800

    Pharm, PharmView, PharmCon
    added Pharmacist, PharmacistView, PharmacistController (but incomplete with errors)

---
+++ src/Pharmacist/Pharmacist.java
@@ -0,0 +1,20 @@
+/*in main uml and entity uml
+contains
++ gethosID()
++ sethosID()
++ getPassword()
++ setPassword()
+extends from <<abstract>>user, related to PharmacistRepository 
+used by PharmacistController
+*/
+package Pharmacist;
+
+import java.util.ArrayList;
+import java.util.List;
+import User.User;
+
+public class Pharmacist extends User{
+    public Pharmacist(String staffID, String password, String role, String name, String gender, int age){
+        super(staffID, password, role, name, gender, age);
+    }
+}
---
+++ src/Pharmacist/PharmacistController.java
@@ -0,0 +1,19 @@
+/*in main uml and controller uml
+contains: 
++ requestRestock() 
++ updateStatus() 
++ updatePrescriptionstatus() 
+uses Pharmacist, InventoryController, ResotckRequestController, AppointmentOutcomeRecordeController
+used by PharmacistView
+*/
+
+package Pharmacist;
+
+
+import Pharmacist.*;
+
+public class PharmacistController{
+    private Pharmacist pharmacist;
+    private PharmacistView view;
+}
+
