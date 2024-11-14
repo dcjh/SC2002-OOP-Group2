c7736ca3d56d962081291826f9989fc6671c389c
sherilyn7 <chuasherilyn247@gmail.com>
2024-11-14 19:58:13 +0800

    update pharmview and change medicineDAO

--- src/View/PharmacistView.java
+++ src/View/PharmacistView.java
@@ -5,3 +5,39 @@ contains
 uses PharmacistController
 */
 package View;
+
+import Model.Medicine;
+import Data.DataAccess.MedicineDAO;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class PharmacistView {
+
+    public void MainMenu() {
+        // Display main menu options
+    }
+
+    public void viewAppointmentOutcome() {
+        // Pharmacist views appointment outcome records to process prescriptions
+    }
+
+    public void viewPrescription() {
+        // Pharmacist views prescription details
+    }
+
+    public void viewInventory() {
+        String filename = "Medicine_List.csv";
+        try {
+            // Read file containing medicine records
+            ArrayList<Medicine> medicines = MedicineDAO.readMedicineList(filename);
+            for (Medicine med : medicines) {
+                System.out.println("Name: " + med.getName());
+                System.out.println("Stock: " + med.getStock());
+                System.out.println("Low Level Stock Alert: " + med.getLLSA());
+            }
+
+        }catch (IOException e) {
+            System.out.println("IOException > " + e.getMessage());
+        }
+    }
+}
