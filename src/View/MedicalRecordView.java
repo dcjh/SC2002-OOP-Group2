d24f2b6054691d0731bb1bf5d2641d9ca04322ef
jaycelimyi <143604419+jaycelimyi@users.noreply.github.com>
2024-11-17 00:42:15 +0800

    update name dao

--- src/View/MedicalRecordView.java
+++ src/View/MedicalRecordView.java
@@ -8,7 +8,7 @@ public class MedicalRecordView {
     public void menu(MedicalRecord medicalRecord) {
         System.out.println("Medical Record:");
         System.out.println("Patient ID: " + medicalRecord.getPatientID());
-        System.out.println("Name: " + medicalRecord.getPatientID());
+        System.out.println("Name: " + medicalRecord.getName());
         System.out.println("Date of Birth: " + medicalRecord.getDob());
         System.out.println("Gender: " + medicalRecord.getGender());
         System.out.println("Phone Number: " + medicalRecord.getPhoneNumber());
