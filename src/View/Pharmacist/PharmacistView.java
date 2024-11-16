77bcc20e667159a0b8a19ecfe4fa4c288a2d6cd6
sherilyn <125991438+sherilyn7@users.noreply.github.com>
2024-11-16 23:46:52 +0800

    Shifted PharmacistView.java into Pharmacist folder
    just added package View.Pharmacist and moved it inside

--- src/View/Pharmacist/PharmacistView.java
+++ src/View/Pharmacist/PharmacistView.java
@@ -1,59 +1,50 @@
-/*in main uml
-contains
-+ viewAppointmentoutcome()
-+ viewPrescription()
-+ viewInventory
-uses PharmacistController
-*/
-package View;
-import java.io.*;
-import java.util.*;
-import Data.Repository.MedicineRepository;
-import Controller.PharmacistController;
+package View.Pharmacist;
+
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
-public class PharmacistView {
+import Controller.AdministratorController;
+import Controller.AppointmentOutcomeController;
+import Controller.InventoryController;
+import Controller.PharmacistController;
+import Model.User;
 
+public class PharmacistView implements UserMainView{
+    private User user;
     private PharmacistController pharmacistController;
-    public void PharmacistViewMenu(String pharmacistID){
-        private Scanner scanner = new Scanner(System.in);
-        public void displayMenu() {
-            int option = 0;
-            while (option!=5) {
-                System.out.println();
-                System.out.println("Pharmacist Menu:");
-                System.out.println("1. View Appointment Outcome Record ");
-                System.out.println("2. Update Prescription Status");
-                System.out.println("3. View Medication Inventory");
-                System.out.println("4. Submit Replenishment Request");
-                System.out.println("5. Logout....");
-                System.out.println();
-                System.out.println("Select an option:");
-                option = scanner.nextInt();
-                switch(option) {
-                    case 1:viewAppointmentoutcome();
-                    case 2:Controller.PharmacistController.updatePrescriptionstatus(null);
-                    case 3:viewMedicationinventory();
-                    case 4:submitReplenishmentrequest();
-                    case 5: break;
-                }
-
-            }
-
-        }
-
-        private void viewAppointmentoutcome() {
-            System.out.println("Functionality in progress");
-        }
-
-        private void viewMedicationinventory() {
-            InventoryController inventoryController = new InventoryController();
-            InventoryView.displayInventory();
-        }
-
-
-        private void submitReplenishmentrequest() {
-            System.out.println("\n--- Submit Replenishment Request ---");
-            PharmacistController.submitReplenishmentRequest();
-        }
+	public PharmacistView(User user) {
+        this.user = user;
+        this.pharmacistController = new PharmacistController();
     }
+
+        public PharmacistView(PharmacistController pharmacistController) {
+        this.pharmacistController = pharmacistController;
+    }
+	
+    private Scanner scanner = new Scanner(System.in);
+    public void displayMenu() {
+    	int option = 0;
+    	while (option!=6) {
+            System.out.println();
+            System.out.println("Pharmacist Menu:");
+            System.out.println("1. View Appointment Outcome Record ");
+            System.out.println("2. Update Prescription Status");
+            System.out.println("3. View Medication Inventory");
+            System.out.println("4. Submit Replenishment Request");
+            System.out.println("5. View Replenishment Request");
+            System.out.println("6. Logout....");
+            System.out.println();
+            System.out.println("Select an option:");
+            option = scanner.nextInt();
+            switch(option) {
+            case 1:pharmacistController.displayAppointmentOutcome(); break;
+            case 2:Controller.PharmacistController.updatePrescriptionstatus(null);break;
+            case 3:pharmacistController.displayInventory();break;
+            case 4:pharmacistController.submitReplenishmentRequests();break;
+            case 5: pharmacistController.viewAllReplenishmentRequests();break;
+            case 6: break;
+    	}  
+     }
+    }
+}
