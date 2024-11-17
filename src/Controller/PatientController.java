00e2e4f872f801bd5fadb969a338059b54755de3
ZCHUA040 <159976032+ZCHUA040@users.noreply.github.com>
2024-11-17 18:17:33 +0800

    Update PatientController.java

--- src/Controller/PatientController.java
+++ src/Controller/PatientController.java
@@ -6,6 +6,7 @@ import java.util.List;
 /*import Controller.AppointmentController;
 import Controller.MedicalRecordController;*/
 import Model.Roles.Patient;
+import Model.Shared.Appointment;
 
 public class PatientController {
     private Patient patient;
